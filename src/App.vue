ad03564d758cb81036cc18c099f49d4f146ec6a1
shrall <shrallvierdo@gmail.com>
2021-11-02 00:22:08 +0700

    facebook pixel

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@ import { useHead } from '@vueuse/head'
 export default {
   setup() {
     useHead({
+      script: [
+        {
+          children: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js%27);fbq('init', '1026971191196625');fbq('track', 'PageView');`,
+        },
+      ],
       meta: [
         {
           name: `description`,
@@ -26,7 +31,8 @@ export default {
         },
         {
           property: 'og:description',
-          content: 'Kartu Tahu adalah sebuah permainan kartu yang berisikan pertanyaan-pertanyaan yang dapat dimainkan bersama teman-temanmu dengan berbagai pilihan tema menarik yang dapat kamu pilih dan mainkan dimana pun dan kapan pun.',
+          content:
+            'Kartu Tahu adalah sebuah permainan kartu yang berisikan pertanyaan-pertanyaan yang dapat dimainkan bersama teman-temanmu dengan berbagai pilihan tema menarik yang dapat kamu pilih dan mainkan dimana pun dan kapan pun.',
         },
         {
           property: 'og:site_name',
